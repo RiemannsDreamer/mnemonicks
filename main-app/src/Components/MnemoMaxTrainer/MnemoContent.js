63587e1ac1cca563543d5f9a64d7b9fdeadf0d5a
mercus <moritz.gehrig@bluewin.ch>
2023-11-22 08:01:01 +0100

    buttons onClick update background

--- main-app/src/Components/MnemoMaxTrainer/MnemoContent.js
+++ main-app/src/Components/MnemoMaxTrainer/MnemoContent.js
@@ -11,7 +11,7 @@ const MnemoContent = () => {
     return (
 
         <div className="w-full h-5/6 bg-gradient-to-tr from-cyan-500 to-cyan-900 rounded-2xl m-4 text-center flex items-center justify-center">
-            <p className={"text-center text-red-600 text-4xl"}>{timeInSeconds}</p>
+            <p className={"text-center text-red-600 text-5xl"}>{timeInSeconds}</p>
         </div>
     );
 }
